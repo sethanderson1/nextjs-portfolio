7e92f2aa807ca5c5791e0c88df5b290fb215af6c
Seth Anderson <sethdanderson17@gmail.com>
2021-04-12 01:01:35 -0700

    c4

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,7 @@ import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div>
+    <div>  
       <Head> 
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
