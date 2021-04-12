4f31d898cdc201a57993f2779382c4f9220e9ab2
Seth Anderson <sethdanderson17@gmail.com>
2021-04-12 00:18:40 -0700

    Some commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className="flex justify-center">
-          <section className="w-6/12 h-96 bg-blue-700 rounded-3xl text-yellow-300 text-center ">
+          <section className="w-5/6 h-96 bg-blue-700 rounded-3xl text-yellow-300 text-center ">
             <div className="border-blue-300 border-solid">
               <h2 className='my-12 text-3xl '>About</h2>
               <p className='text-yellow-50'>I'm Seth. That's me</p>
